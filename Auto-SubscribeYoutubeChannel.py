5eb4dcf4a54fe9bb53308e9796565747e7dc42c5
Raunak Kumar <71320898+raunakmishraa@users.noreply.github.com>
2024-07-02 21:59:17 +0545

    Add files via upload

---
+++ Auto-SubscribeYoutubeChannel.py
@@ -0,0 +1,12 @@
+import pyautogui
+pyautogui.moveTo(255,1075,duration=2)
+pyautogui.click()
+pyautogui.typewrite('chrome')
+pyautogui.typewrite(['enter'])
+pyautogui.moveTo(970,100,duration=2)
+pyautogui.click()
+pyautogui.typewrite('https://www.youtube.com/channel/UChLNiQTld_EJOHaKqB3BDSg')
+pyautogui.typewrite(['enter'])
+pyautogui.moveTo(1325,308,duration=2)
+pyautogui.click()
+print(pyautogui.position())
